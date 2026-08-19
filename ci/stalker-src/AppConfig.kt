ae2be5063995bce3ecc93bff1e645d10c781531c
alfredi1980 <103353014+alfredi1980@users.noreply.github.com>
2026-08-19 19:08:53 +0200

    Add Stalker AppConfig source

---
+++ ci/stalker-src/AppConfig.kt
@@ -0,0 +1,9 @@
+package al.speedline.iptv
+
+object AppConfig {
+    const val SERVER_BASE_URL = "http://lok.ipalfa.org:25461"
+    const val STALKER_PORTAL_URL = "http://lok.ipalfa.org:25461/c"
+    const val SYNC_INTERVAL_HOURS = 24L
+    const val APP_USER_AGENT = "Mozilla/5.0 (QtEmbedded; U; Linux; C) AppleWebKit/533.3 (KHTML, like Gecko) MAG254 stbapp SpeedlineIPTV"
+    const val STB_USER_AGENT = "Model: MAG254; Link: Ethernet"
+}
